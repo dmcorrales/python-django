042673fd60d7447c37121fd0ee4a34fc1d8885f5
Daniel Corrales <dmcorrales@unbosque.edu.co>
2020-08-11 17:34:06 -0500

    Model created

--- courses/views.py
+++ courses/views.py
@@ -5,3 +5,5 @@ from.models import Course
 
 def course_list(request):
     courses = Course.objects.all()
+    response = ", ".join([str(course) for course in courses])
+    return HttpResponse(response)
