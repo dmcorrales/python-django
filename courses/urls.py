834ab307b2791abb27e44db1cf72c4c80785a5ac
Daniel Corrales <dmcorrales@unbosque.edu.co>
2020-08-12 14:47:20 -0500

    Detaisl

--- courses/urls.py
+++ courses/urls.py
@@ -6,5 +6,5 @@ app_name = 'courses'
 urlpatterns = [
     path('', views.course_list, name='course_list'),
     path('<int:pk>/', views.course_detail,  name='course_detail'),
-    path('<int:course_pk>/<int:lesson_pk>/', views.lesson_detail),
+    path('<int:course_pk>/<int:lesson_pk>/', views.lesson_detail, name='lesson_detail'),
 ]
