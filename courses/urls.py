f4eb37cbeddef70078ea3e22185e754fb62bb2e0
Daniel Corrales <dmcorrales@unbosque.edu.co>
2020-08-12 14:42:04 -0500

    namespaces

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'courses'
+
 urlpatterns = [
     path('', views.course_list, name='course_list'),
-    path('<int:pk>/', views.course_detail),
+    path('<int:pk>/', views.course_detail,  name='course_detail'),
     path('<int:course_pk>/<int:lesson_pk>/', views.lesson_detail),
 ]
