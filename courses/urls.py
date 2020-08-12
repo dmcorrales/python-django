d0e761b021b1123cab514792b36e8af027b8197f
Daniel Corrales <dmcorrales@unbosque.edu.co>
2020-08-12 13:03:28 -0500

    Details

--- courses/urls.py
+++ courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.course_list),
     path('<int:pk>/', views.course_detail),
+    path('lesson/<int:pk>/', views.lesson_detail),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from.models import Course
+from.models import Course, Lesson
 
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', {'courses': courses})
 
 def course_detail(request,pk):
-    courses = Course.objects.get(pk=pk)
+    #courses = Course.objects.get(pk=pk)
+    courses = get_object_or_404(Course, pk=pk)
     return render(request, 'courses/course_detail.html', {'course': courses})
+
+
+def lesson_detail(request,pk):
+    lessons = get_object_or_404(Lesson, pk=pk)
+    return render(request,'courses/lesson_detail.html', {'lesson': lessons})
+
